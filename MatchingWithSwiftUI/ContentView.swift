0b11302b4d34196079959c042d39912d873bdb5b
tkt12 <kinotaka1227@gmail.com>
2025-06-07 16:31:22 +0900

    Initial Commit

---
+++ MatchingWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 木下喬仁 on 2025/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchingWithSwiftUI/MatchingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingWithSwiftUIApp.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 木下喬仁 on 2025/06/07.
+//
+
+import SwiftUI
+
+@main
+struct MatchingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
