a00f1b9e1c117ab5f344e6bb5bb22a8fced2da9a
tkt12 <kinotaka1227@gmail.com>
2025-06-16 15:43:17 +0900

    first commit

---
+++ MatchingWithSwiftUI/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 木下喬仁 on 2025/06/11.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ MatchingWithSwiftUI/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 木下喬仁 on 2025/06/08.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
