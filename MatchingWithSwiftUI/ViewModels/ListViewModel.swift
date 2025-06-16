7f8c8a12c6ab1a1f5fcce965a7b845984f72849b
tkt12 <kinotaka1227@gmail.com>
2025-06-16 18:48:31 +0900

    feat: データ制御のためのクラスを作成

---
+++ MatchingWithSwiftUI/ViewModels/ListViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewModel.swift
+//  MatchingWithSwiftUI
+//
+//  Created by 木下喬仁 on 2025/06/16.
+//
+
+import Foundation
+
+class ListViewModel {
+    
+    var users = [User]()
+    
+    init() {
+        self.users = getMockUsers()
+    }
+    
+    private func getMockUsers() -> [User] {
+        return [
+            User.MOCK_USER1,
+            User.MOCK_USER2,
+            User.MOCK_USER3,
+            User.MOCK_USER4,
+            User.MOCK_USER5,
+            User.MOCK_USER6,
+            User.MOCK_USER7,
+        ]
+    }
+}
