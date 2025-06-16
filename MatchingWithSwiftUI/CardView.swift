416e7723ffb9c7836c85f366484af01e7c2df928
tkt12 <kinotaka1227@gmail.com>
2025-06-16 15:43:33 +0900

    first commit

--- MatchingWithSwiftUI/CardView.swift
+++ MatchingWithSwiftUI/CardView.swift
@@ -8,11 +8,97 @@
 import SwiftUI
 
 struct CardView: View {
+    
+    @State private var offset: CGSize = .zero
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            // Background
+            Color.black
+            
+            // image
+            imageLayer
+            
+            // Gradient
+            LinearGradient(colors: [.clear, .black], startPoint: .center, endPoint: .bottom)
+            
+            // Infomation
+            informationLayer
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .offset(offset)
+        .gesture(gesture)
     }
 }
 
 #Preview {
-    CardView()
+    ListView()
+}
+
+// MARK: -UI
+extension CardView {
+    
+    private var imageLayer: some View {
+        Image("user01")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 100)
+    }
+    
+    private var informationLayer: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .bottom) {
+                Text("ブルー")
+                    .font(.largeTitle.bold())
+                
+                Text("99")
+                    .font(.title)
+                Image(systemName: "checkmark.seal.fill")
+                    .foregroundStyle(.white, .blue)
+                    .font(.title2)
+            }
+            
+            Text("よろしくお願いします")
+        }
+        .foregroundStyle(.white)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
+// MARK: -Action
+extension CardView {
+    
+    private var gesture: some Gesture {
+        DragGesture()
+            .onChanged{ value in
+                let width = value.translation.width
+                let height = value.translation.height
+                
+//                var limitedHeight: CGFloat = 0
+//                
+//                if (height > 0) {
+//                    if (height > 100) {
+//                        limitedHeight = 100
+//                    } else {
+//                        limitedHeight = height
+//                    }
+//                } else {
+//                    if (height < -100) {
+//                        limitedHeight = -100
+//                    } else {
+//                        limitedHeight = height
+//                    }
+//                }
+                
+                let limitedHeight = height > 0 ? min(height, 100) : max(height, -100)
+                
+                offset = CGSize(width: width, height: limitedHeight)
+            }
+            .onEnded { value in
+                withAnimation(.smooth) {
+                    offset = .zero
+                }
+            }
+    }
 }
--- MatchingWithSwiftUI/ListView.swift
+++ MatchingWithSwiftUI/ListView.swift
@@ -9,10 +9,72 @@ import SwiftUI
 
 struct ListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            // Cards
+            cards
+            
+            // Actions
+            actions
+        }
+        .background(.black, in: RoundedRectangle(cornerRadius: 15))
+        .padding(.horizontal, 6)
     }
 }
 
 #Preview {
     ListView()
+}
+
+extension ListView {
+    
+    private var cards: some View {
+        ZStack {
+            ForEach(0..<1) { _ in
+                CardView()
+            }
+        }
+    }
+    
+    private var actions: some View {
+        HStack(spacing: 68) {
+            Button {
+                print("ボタンがタップされました")
+            } label: {
+                Image(systemName: "xmark")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundStyle(.red)
+                    .background {
+                        Circle()
+                            .stroke(.red, lineWidth: 1)
+                            .frame(width: 60, height: 60)
+                    }
+            }
+            Button {
+                print("ボタンがタップされました")
+            } label: {
+                Image(systemName: "arrow.counterclockwise")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundStyle(.yellow)
+                    .background {
+                        Circle()
+                            .stroke(.yellow, lineWidth: 1)
+                            .frame(width: 50, height: 50)
+                    }
+            }
+            Button {
+                print("ボタンがタップされました")
+            } label: {
+                Image(systemName: "heart")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundStyle(.mint)
+                    .background {
+                        Circle()
+                            .stroke(.mint, lineWidth: 1)
+                            .frame(width: 60, height: 60)
+                    }
+            }
+        }
+        .foregroundStyle(.white)
+        .frame(height: 100)
+    }
 }
